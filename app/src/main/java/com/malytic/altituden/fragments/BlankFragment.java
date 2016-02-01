12407b2a82c47104c3526b18edd024b4da50aebf
medevag <michaeledevag@gmail.com>
2016-02-01 11:52:11 +0100

    Added skeleton

---
+++ app/src/main/java/com/malytic/altituden/fragments/BlankFragment.java
@@ -0,0 +1,21 @@
+package com.malytic.altituden.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.malytic.altituden.R;
+
+
+public class BlankFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/malytic/altituden/fragments/MapsFragment.java
@@ -0,0 +1,74 @@
+package com.malytic.altituden.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.malytic.altituden.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+//    private MapView mMapView;
+//    private GoogleMap googleMap;
+
+/*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_fragment);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+*/
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        Toast.makeText(getActivity(), "This is a map", Toast.LENGTH_LONG).show();
+
+        View view = inflater.inflate(R.layout.activity_maps_fragment, null, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        // Inflate the layout for this fragment
+        return view; // inflater.inflate(R.layout.activity_maps_fragment, container, false);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(getActivity(), "Map redy, onMapReadyCallback called", Toast.LENGTH_LONG).show();
+
+        mMap = googleMap;
+
+        //LocationManager manager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        //Location lastLocation = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        LatLng sydney = new LatLng(57.706389, 11.938270);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Chalmers"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+    }
+}
