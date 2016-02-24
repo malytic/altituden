2fcf8cc9d9ff2c220a693068c0cd16beeedb2730
medevag <michaeledevag@gmail.com>
2016-02-24 20:12:49 +0100

    seems to be working
    Commented code. If you open the map and then deny locations it will not
    crash. The mapFragment will remove itself and instead add a
    GraphFragment with the tag “crash”

--- app/src/main/java/com/malytic/altituden/MainActivity.java
+++ app/src/main/java/com/malytic/altituden/MainActivity.java
@@ -24,8 +24,6 @@ import com.malytic.altituden.fragments.FormFragment;
 import com.malytic.altituden.fragments.GraphFragment;
 import com.malytic.altituden.fragments.MapsFragment;
 
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -33,7 +31,7 @@ public class MainActivity extends AppCompatActivity
     private GraphFragment graphFragment;
     private FormFragment formFragment;
     private boolean firstRun;
-    private boolean permissionsGranted = false;
+
     public static PathData pathData;
     int weight;
     int age;
@@ -43,12 +41,14 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //Get variables, -1 if they do not exist
         weight = preferences.getInt("weight", -1);
         age = preferences.getInt("age", -1);
         gender = preferences.getInt("gender",-1);
 
+        //Set firstRun true if permissions is not already granted
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -70,9 +70,8 @@ public class MainActivity extends AppCompatActivity
 
         pathData = new PathData();
 
-        if(firstRun || (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED))
+        //Show dialog about permissions if first time app runs
+        if(firstRun)
             showInformationDialog();
 
         initiateFragments();
@@ -81,43 +80,21 @@ public class MainActivity extends AppCompatActivity
     /**
      * Checks if the app has the correct permissions
      * Otherwise ask for them
+     * Code from:
+     *      http://developer.android.com/training/permissions/requesting.html
      */
     public void askPermission(){
-        /*---------------------------------
-        Ask permission to use GPS
-        */
-
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
-
-            // Should we show an explanation?
-            // if (ActivityCompat.shouldShowRequestPermissionRationale(
-           //          this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-
-            // Show an expanation to the user *asynchronously* -- don't block
-            // this thread waiting for the user's response! After the user
-            // sees the explanation, try again to request the permission.
-
-            //} else {
-            // No explanation needed, we can request the permission.
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 5);
-
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
-            //}
         }
     }
 
     /**
      * Callback from permission ask
-     * @param requestCode
-     * @param permissions
-     * @param grantResults
      */
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
@@ -126,15 +103,12 @@ public class MainActivity extends AppCompatActivity
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    permissionsGranted = true;
                     // permission was granted, yay!
-
                 } else {
                     // permission denied, boo!
+                    //Show same informationdialog as if firstRun
                     showInformationDialog();
                 }
-                return;
             }
         }
     }
@@ -173,6 +147,8 @@ public class MainActivity extends AppCompatActivity
         formFragment = new FormFragment();
         transaction.add(R.id.frame, formFragment);
 
+        //If not first time app runs a map should be the first screen
+        //Create map, set it as current fragment and hide formFragment
         if(!firstRun){
             transaction.hide(formFragment);
             mapsFragment = new MapsFragment();
@@ -229,8 +205,13 @@ public class MainActivity extends AppCompatActivity
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
         if (id == R.id.nav_maps) {
+            //Hide all other fragments
             transaction.hide(graphFragment);
             transaction.hide(formFragment);
+            //A fragment with the tag "crash" is present if the map has
+            //crashed some time. Hide it if it exists
+            if(getSupportFragmentManager().findFragmentByTag("crash") != null)
+                transaction.hide(getSupportFragmentManager().findFragmentByTag("crash"));
 
             if(firstRun){
                 mapsFragment = new MapsFragment();
@@ -242,7 +223,13 @@ public class MainActivity extends AppCompatActivity
         }
 
         else if (id == R.id.nav_graph) {
+            //Hide all other fragments
             transaction.hide(formFragment);
+            //A fragment with the tag "crash" is present if the map has
+            //crashed some time. Hide it if it exists
+            if(getSupportFragmentManager().findFragmentByTag("crash") != null)
+                transaction.hide(getSupportFragmentManager().findFragmentByTag("crash"));
+
             if(mapsFragment != null)
                 transaction.hide(mapsFragment);
 
@@ -253,8 +240,15 @@ public class MainActivity extends AppCompatActivity
         }
 
         else if (id == R.id.nav_form) {
+            //Hide all other fragments
             transaction.hide(graphFragment);
-            transaction.hide(mapsFragment);
+            //A fragment with the tag "crash" is present if the map has
+            //crashed some time. Hide it if it exists
+            if(getSupportFragmentManager().findFragmentByTag("crash") != null)
+                transaction.hide(getSupportFragmentManager().findFragmentByTag("crash"));
+
+            if(mapsFragment != null)
+                transaction.hide(mapsFragment);
 
             if(formFragment.isHidden()){
                 transaction.show(formFragment);
