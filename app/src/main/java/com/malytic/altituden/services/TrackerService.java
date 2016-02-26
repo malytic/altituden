f2805c2eb17793a87b6c7be64cf8b52c1e424acb
Magnus Rising <malytic@users.noreply.github.com>
2016-02-26 12:12:14 +0100

    Improve tracking status with persistance

--- app/src/main/java/com/malytic/altituden/services/TrackerService.java
+++ app/src/main/java/com/malytic/altituden/services/TrackerService.java
@@ -3,10 +3,12 @@ package com.malytic.altituden.services;
 import android.app.Service;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -22,27 +24,45 @@ import com.google.android.gms.location.LocationServices;
  */
 public class TrackerService extends Service implements LocationListener {
 
+    public static final String TRACKING_STATUS = "trackingStatus"; // Key for shared preferences
+    public static final String TRACKING_TIME = "trackingTime"; // Key for shared preferences
+
     private static final String TAG = TrackerService.class.getSimpleName();
+    private static final long TRACKING_TIME_DIFF = 30000; // Time before tracking expires
     private static final long UPDATE_INTERVAL = 2000; // Minimum time between location updates
     private final IBinder mBinder = new LocalBinder(); // Binder given to clients of the service
 
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
-    private boolean mIsTracking = false;
+    private SharedPreferences.Editor mEditor;
+    private SharedPreferences mPreferences;
+    private boolean mIsTracking;
 
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate");
         super.onCreate();
+
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(UPDATE_INTERVAL);
         mLocationRequest.setFastestInterval(UPDATE_INTERVAL);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(new GoogleApiHandler())
                 .addOnConnectionFailedListener(new GoogleApiHandler())
                 .addApi(LocationServices.API).build();
-        if (mIsTracking) {
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+        mIsTracking = mPreferences.getBoolean(TRACKING_STATUS, false);
+        long mTrackingTime = mPreferences.getLong(TRACKING_TIME, 0);
+        boolean mTrackingExpired = System.currentTimeMillis() < (mTrackingTime+TRACKING_TIME_DIFF);
+        Log.d(TAG, "Previous tracking status: "+ mIsTracking);
+        Log.d(TAG, "Previous tracking time: "+ mTrackingTime);
+        Log.d(TAG, "Has tracking expired: "+ mTrackingExpired);
+
+        if (mIsTracking && !mTrackingExpired) {
             mGoogleApiClient.connect();
         }
     }
@@ -56,6 +76,8 @@ public class TrackerService extends Service implements LocationListener {
     @Override
     public void onLocationChanged(Location location) {
         Log.d(TAG, "onLocationChanged: " + location);
+        mEditor.putLong(TRACKING_TIME, System.currentTimeMillis());
+        mEditor.commit();
         // save in database
     }
 
@@ -80,6 +102,8 @@ public class TrackerService extends Service implements LocationListener {
     public void startTracking() {
         Log.d(TAG, "startTracking called");
         mIsTracking = true;
+        mEditor.putBoolean(TRACKING_STATUS, mIsTracking);
+        mEditor.commit();
         if (mGoogleApiClient.isConnected()) {
             requestLocationUpdates();
             // create new row in database
@@ -94,6 +118,8 @@ public class TrackerService extends Service implements LocationListener {
     public void stopTracking() {
         Log.d(TAG, "stopTracking called");
         mIsTracking = false;
+        mEditor.putBoolean(TRACKING_STATUS, mIsTracking);
+        mEditor.commit();
         if (mGoogleApiClient.isConnected()) {
             cancelLocationUpdates();
             mGoogleApiClient.disconnect();
@@ -143,7 +169,7 @@ public class TrackerService extends Service implements LocationListener {
 
         @Override
         public void onConnectionFailed(ConnectionResult connectionResult) {
-            mIsTracking = false;
+            stopTracking();
             if (connectionResult.hasResolution()) {
                 try {
                     // Start an Activity that tries to resolve the error
